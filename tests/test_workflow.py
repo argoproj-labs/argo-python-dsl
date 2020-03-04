0eb0afc9c08537d52c332b68fa877f8274067c94
Marek Cermak <macermak@redhat.com>
2020-03-04 10:33:19 +0100

    fix: usr: Fixed Workflow.submit parameter handling
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   argo/workflows/dsl/_base.py
    modified:   argo/workflows/dsl/_workflow.py
    modified:   tests/test_workflow.py

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -2,6 +2,9 @@ import flexmock
 import pytest
 import requests
 
+from argo.workflows.client import V1alpha1Api
+from argo.workflows.client.models import V1alpha1Arguments, V1alpha1Parameter
+
 from argo.workflows.dsl import Workflow
 
 from ._base import TestCase
@@ -10,8 +13,25 @@ from ._base import TestCase
 
 
 @pytest.fixture  # type: ignore
+def api() -> V1alpha1Api:
+    """Fake API client."""
+    return V1alpha1Api()
+
+
+@pytest.fixture  # type: ignore
 def url() -> str:
     """Fake URL fixture."""
+
+
+@pytest.fixture  # type: ignore
+def wf() -> Workflow:
+    """Fake Workflow."""
+
+    class FakeWorkflow(Workflow):
+        name = "test"
+
+    wf = FakeWorkflow(compile=True)
+    return wf
 
 
 class TestWorkflow(TestCase):
@@ -20,7 +40,7 @@ class TestWorkflow(TestCase):
     _WORKFLOW_FILE = TestCase.DATA / "workflows" / "hello-world.yaml"
 
     def test_from_file(self) -> None:
-        """Test `Workflow.from_file` methods."""
+        """Test `Workflow.from_file` method."""
         wf = Workflow.from_file(self._WORKFLOW_FILE)
 
         assert isinstance(wf, Workflow)
@@ -29,7 +49,7 @@ class TestWorkflow(TestCase):
         assert len(wf.spec.templates) == 1
 
     def test_from_url(self, url: str) -> None:
-        """Test `Workflow.from_url` methods."""
+        """Test `Workflow.from_url` method."""
         fake_response = type(
             "Response",
             (),
@@ -43,3 +63,33 @@ class TestWorkflow(TestCase):
         assert wf.name == "test"
         assert wf.kind == "Workflow"
         assert len(wf.spec.templates) == 1
+
+    def test_submit(self, api: V1alpha1Api, wf: Workflow) -> None:
+        """Test `Workflow.submit` method."""
+        fake_workflow_name = "test"
+        flexmock(V1alpha1Api).should_receive("create_namespaced_workflow").and_return(
+            fake_workflow_name
+        )
+
+        # submit w/o parameters
+        workflow_name: str = wf.submit(client=V1alpha1Api(), namespace="test")
+
+        assert isinstance(workflow_name, str)
+        assert workflow_name == "test"
+
+        # submit w/ parameters
+        with pytest.raises(AttributeError) as exc:
+            # Expected ValueError due to undefined parameter
+            workflow_name: str = wf.submit(
+                client=api, namespace="test", parameters={"param": "test"}
+            )
+
+        wf.spec.arguments = V1alpha1Arguments(
+            parameters=[V1alpha1Parameter(name="param")]
+        )
+        workflow_name: str = wf.submit(
+            client=api, namespace="test", parameters={"param": "test"}
+        )
+
+        assert isinstance(workflow_name, str)
+        assert workflow_name == "test"
